1f964e0500c4ed1245db050b8da662d7d1491aca
Twincasper <justinwatson651@gmail.com>
2023-10-04 14:12:22 -0400

    Wrote third function

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ const multiplesOfThree = () => {
         }
     }
 }
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 3; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
+}
+console.log(multiplesOfThreeOrFive());
